c089b2df42705d2a7595eaf32938846a837cef0a
esibaja0795 <sibaja0795@gmail.com>
2025-06-25 16:56:41 -0600

    change route
    change route

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     output: 'static',
-    base: '/Tech-Crew/',
+    base: '/TechCrew/',
 });
